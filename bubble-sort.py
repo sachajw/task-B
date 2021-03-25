df57bfb8e8857fcab93596b0ed00b1eac10fdd2c
Sacha Wharton <sachajw@gmail.com>
2021-03-25 15:13:33 +0200

    added requirements

--- bubble-sort.py
+++ bubble-sort.py
@@ -1,45 +1,44 @@
 
 import os
+import json
+import sys
 
-from flask import Flask
+from flask import Flask, render_template, jsonify, request
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-api = Bubble_Sort(app)
+#api = Bubble_Sort(app)
 
 # Python3 Bubble sort
-class Bubble_Sort(Resource):
-    def bubbleSort(arr):
-        n = len(arr)
-    
-        # Traverse through all array elements
-        for i in range(n):
-            swapped = False
-    
-            # Last i elements are already in place
-            for j in range(0, n-i-1):
-            
-                # traverse the array from 0 to
-                # n-i-1. Swap if the element 
-                # found is greater than the
-                # next element
-                if arr[j] > arr[j+1] :
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
-                    swapped = True
-    
-            # IF no two elements were swapped
-            # by inner loop, then break
-            if swapped == False:
-                break
-            
-    # number sequence to test above
-    arr = [66, 37, 23, 17, 27, 14, 95]
-    
-    bubbleSort(arr)
-    
-    print ("Sorted array :")
-    for i in range(len(arr)):
-        print ("%d" %arr[i],end=" ")
+@app.route('/', methods=['GET'])
+
+def bubbleSort(arr):
+    n = len(arr)
+   
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+  
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+   
+            # traverse the array from 0 to
+            # n-i-1. Swap if the element 
+            # found is greater than the
+            # next element
+            if arr[j] > arr[j+1] :
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+  
+        # IF no two elements were swapped
+        # by inner loop, then break
+        if swapped == False:
+            break
+           
+# number sequence to test above
+arr = [66, 37, 23, 17, 27, 14, 95]
+   
+bubbleSort(arr)
 
 api.add_resource(Bubble_Sort, '/')
 
